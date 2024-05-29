a6a746dd71c28b97be8ff2cb1d46682503c59bcf
rukwarocynthia4093@gmail.com <rukwarocynthia4093@gmail.com>
2024-05-29 09:41:22 +0300

    done

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,12 @@
 from user import User
 
 class Student(User):
+
+    def __init__(self, first_name, last_name, knowledge = []):
+        super().__init__(first_name, last_name)
+        self.knowledge = knowledge
     
-    def learn(self):
-        pass
+    def learn(self, new_information):
+        self.knowledge.append(new_information)
+
+      
